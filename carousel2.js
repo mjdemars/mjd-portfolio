30936b58b3cd03c41d246c2159dc36bffeb75b87
mjdemars <mdemars7@gatech.edu>
2021-11-25 17:38:48 -0500

    Update to links and styling

--- carousel2.js
+++ carousel2.js
@@ -2,18 +2,18 @@ var slideIndex2 = 0;
 showSlides2();
 
 function showSlides2() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides2");
-    var dots = document.getElementsByClassName("dot2");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    var j;
+    var slides2 = document.getElementsByClassName("mySlides2");
+    var dots2 = document.getElementsByClassName("dot2");
+    for (j = 0; j < slides2.length; j++) {
+      slides2[j].style.display = "none";  
     }
     slideIndex2++;
-    if (slideIndex2 > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    if (slideIndex2 > slides2.length) {slideIndex2 = 1}    
+    for (j = 0; j < dots2.length; j++) {
+      dots2[j].className = dots2[j].className.replace(" active", "");
     }
-    slides[slideIndex2-1].style.display = "block";  
-    dots[slideIndex2-1].className += " active";
+    slides2[slideIndex2-1].style.display = "block";  
+    dots2[slideIndex2-1].className += " active";
     setTimeout(showSlides2, 3000); // Change image every 3 seconds
 }
